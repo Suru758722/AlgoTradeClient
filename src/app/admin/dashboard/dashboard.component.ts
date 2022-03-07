0fd7519754719c96a9046852d3fdf252d8566ccc
suraj mandal <suru9406231@gmail.com>
2022-03-07 04:03:03 -0800

    commit

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 declare var LightweightCharts: any
 declare var require: any
 const dotnetify: any = require('dotnetify');
 dotnetify.hubServerUrl = 'http://localhost:46194';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,50 +14,62 @@ export class DashboardComponent implements OnInit {
 
   state: any = {};
   vm: any;
- title = 'NSEApp';
- lineSeries : any
- data: any[]= [{time: 1556877600, value: 17100}]
-constructor(){
- this.vm = dotnetify.connect('LiveChartVM', {      
-   getState: () => this.state,
-   setState: (state: any) => this.updateData(state)
- });
-}
+  title = 'NSEApp';
+  lineSeries: any
+  data: any[] = [{ time: 1556877600, value: 17100 }]
+  dispatch: any;
+  constructor() {
+  //   interval(5000).subscribe(x => {
+  //     this.getChartData();
+  // });
+  this.vm = dotnetify.connect('LiveChartVM', {      
+    getState: () => this.state,
+    setState: (state: any) =>  this.updateData(state)
+  });
+  
+  
+  }
+  changeTimeFrame(){
+    this.state.TimeFrame = 'Day'
+    this.vm.$dispatch(this.state)
+    this.vm.$dispatch({UpdateData: 'name'})
+  }
+  updateData(state) {
+    Object.assign(this.state, state) 
+    const now = Date(this.state.Time)
+    this.lineSeries.update({ time: now, value: this.state.Chart })
+  }
+  ngOnInit() {
+  
+    var chart = LightweightCharts.createChart(document.getElementById('chart'), {
+      width: 600,
+      height: 300,
+      layout: {
+        backgroundColor: '#ffffff',
+        textColor: 'rgba(33, 56, 77, 1)',
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(197, 203, 206, 0.7)',
+        },
+        horzLines: {
+          color: 'rgba(197, 203, 206, 0.7)',
+        },
+      },
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+      },
+    });
 
-updateData(state: any){
+    this.lineSeries = chart.addLineSeries();
+    this.lineSeries.setData(this.data)
 
- const now = Date.now();
- 
- this.data.push({time: now, value: state.Chart[0]})
- this.lineSeries.update({time: now, value: state.Chart[0]})
+    
+  }
 
- }
-ngOnInit(){
- var chart = LightweightCharts.createChart(document.getElementById('chart'), {
-   width: 600,
-   height: 300,
-   layout: {
-     backgroundColor: '#ffffff',
-     textColor: 'rgba(33, 56, 77, 1)',
-   },
-   grid: {
-     vertLines: {
-       color: 'rgba(197, 203, 206, 0.7)',
-     },
-     horzLines: {
-       color: 'rgba(197, 203, 206, 0.7)',
-     },
-   },
-   timeScale: {
-     timeVisible: true,
-     secondsVisible: false,
-   },
- });
- 
- this.lineSeries = chart.addLineSeries();
- this.lineSeries.setData(this.data)
- 
- 
+ getChartData(){
+  
 }
 
 }
